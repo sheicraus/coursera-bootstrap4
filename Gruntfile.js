f776e3c89aaf1f1216d4b99c81de33eac53316f1
sheicraus <sheilamaecraus247@gmail.com>
2021-11-10 23:23:06 +0800

    add: grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const sass = require('node-sass');
+
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt, {
+        sprite: 'grunt-spritesmith',
+        foo: '@abc/grunt-foo',        // for private modules.
+        bar: 'custom/bar.js'          // for custom tasks.
+    });
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    implementation: sass,
+                    outputStyle: 'compact'
+                },
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
