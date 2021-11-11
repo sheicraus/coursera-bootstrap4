2105de286c182ba945abba0ce76012cd64f8b119
sheicraus <sheilamaecraus247@gmail.com>
2021-11-11 21:54:32 +0800

    add: gulp 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,99 @@
-'use strict';
+'use strict'
 
-// Load in all the Gulp plugins
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
-// Adding Gulp Task for SASS
+// Compiling CSS -> SCSS
 gulp.task('sass', function () {
     return gulp.src('./css/*.scss')
-        .pipe(sass().toString('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('.//css'));
 });
 
-// Adding Gulp Task for watching files related to 'sass' task
+// Watch
 gulp.task('sass:watch', function () {
     gulp.watch('./css/*.scss', ['sass']);
 });
 
-// Adding Gulp Task for browser sync
+// Browser sync
 gulp.task('browser-sync', function () {
     var files = [
         './*.html',
         './css/*.css',
-        './js/*.js',
-        './img/*.{png,jpg,gif}'
+        './img/*.{png,jpg,gif}',
+        './js/*.js'
     ];
 
     browserSync.init(files, {
         server: {
-            baseDir: './'
+            baseDir: "./"
         }
     });
+
 });
 
-// I had to change this because of version issue
+// Default task
 gulp.task('default', gulp.series('browser-sync', function () {
     gulp.start('sass:watch');
 }));
+
+
+// Clean
+gulp.task('clean', function () {
+    return del(['dist']);
+});
+
+// Copy fonts
+gulp.task('copyfonts', function () {
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function () {
+    return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+// Usemin
+gulp.task('usemin', function () {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function (stream, file) {
+            return stream
+                .pipe(usemin({
+                    css: [rev()],
+                    html: [function () {
+                        return htmlmin({
+                            collapseWhitespace: true
+                        })
+                    }],
+                    js: [uglify(), rev()],
+                    inlinejs: [uglify()],
+                    inlinecss: [cleanCss(), 'concat']
+                }))
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+
+// Build
+gulp.task('build', gulp.series(
+    'clean',
+    'copyfonts',
+    'imagemin',
+    'usemin'
+));
